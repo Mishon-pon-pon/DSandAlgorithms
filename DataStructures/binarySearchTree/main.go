f3796976e15f9a0c7ce09c4961988eccfa57aa4d
Mishon-pon-pon <terem848@gmail.com>
2020-01-01 23:02:07 +0300

    binarySearchTree research

---
+++ DataStructures/binarySearchTree/main.go
@@ -0,0 +1,87 @@
+package main
+
+import "fmt"
+
+//BST - binarySearchTree
+type BST struct {
+	value     []int
+	left      []int
+	right     []int
+	leftFree  int
+	rightFree int
+	root      int
+	index     int
+	free      int
+}
+
+func (bst *BST) add(params ...int) {
+	var val, root, index int
+	switch len(params) {
+	case 1:
+		val = params[0]
+		root = bst.root
+		index = bst.index
+		break
+	case 2:
+		val = params[0]
+		root = params[1]
+		index = bst.index
+		break
+	case 3:
+		val = params[0]
+		root = params[1]
+		index = params[2]
+		break
+	}
+
+	if bst.root == 0 {
+		bst.root = val
+		bst.value = append(bst.value, val)
+	} else {
+		if root > val {
+			if bst.left[index] == 0 {
+				bst.value = append(bst.value, val)
+				bst.left[index] = bst.free
+				bst.free++
+				bst.index = 0
+				return
+			} else {
+				bst.add(val, bst.value[bst.left[index]], bst.left[index])
+			}
+		} else {
+			if bst.right[index] == 0 {
+				bst.value = append(bst.value, val)
+				bst.right[index] = bst.free
+				bst.free++
+				bst.index = 0
+				return
+			} else {
+				bst.add(val, bst.value[bst.right[index]], bst.right[index])
+			}
+		}
+	}
+
+}
+
+func test(params ...int) {
+	fmt.Println(params)
+}
+
+func main() {
+	var tree BST
+	tree.right = make([]int, 10)
+	tree.left = make([]int, 10)
+	tree.free = 1
+	// tree.value = make([]int, 10)
+	tree.add(5)
+	tree.add(7)
+	tree.add(8)
+	tree.add(6)
+	tree.add(3)
+	tree.add(4)
+	tree.add(2)
+	fmt.Println("ind", []int{0, 1, 2, 3, 4, 5, 6})
+	fmt.Println("val", tree.value)
+	fmt.Println("rig", tree.right)
+	fmt.Println("lef", tree.left)
+}
